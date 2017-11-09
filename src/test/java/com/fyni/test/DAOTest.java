fc59520e693ac37705dc405e166100e13c57c568
frannacdh <frannacdh@gmail.com>
2017-11-09 14:19:48 +0900

    ver 4.1
    added before controller

--- src/test/java/com/fyni/test/DAOTest.java
+++ src/test/java/com/fyni/test/DAOTest.java
@@ -21,17 +21,11 @@ public class DAOTest {
  
 	
 	@Autowired
-	DataSource dataSource;
-	
-	
-	@Autowired
 	@Qualifier("userDao")
 	private UserDAO dao;
 	
 	@Test
 	public void testRead() throws Exception{
 		System.out.println(dao.userRead("CDH"));
-		System.out.println(dataSource);
-		System.out.println(dao);
 	}
 }
