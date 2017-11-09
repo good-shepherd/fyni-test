5f87920b79f824f23ff6855ccc512d33a80e5a02
August <dazedbj@gmail.com>
2017-11-09 14:38:28 +0900

    ver 0.4

--- src/test/java/com/fyni/test/DAOTest.java
+++ src/test/java/com/fyni/test/DAOTest.java
@@ -1,26 +1,17 @@
 package com.fyni.test;
 
-<<<<<<< HEAD
 import java.sql.Connection;
 import java.sql.SQLException;
 
-=======
->>>>>>> master
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-<<<<<<< HEAD
-=======
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
->>>>>>> master
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-<<<<<<< HEAD
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
 @WebAppConfiguration
@@ -47,24 +38,4 @@ public class DAOTest {
 		}
 		System.out.println(con);
 	}
-
-=======
-import com.fyni.user.UserDAO;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml",
-		"file:src/main/webapp/WEB-INF/spring/**/servlet-context.xml"})
-@WebAppConfiguration
-public class DAOTest {
- 
-	
-	@Autowired
-	@Qualifier("userDao")
-	private UserDAO dao;
-	
-	@Test
-	public void testRead() throws Exception{
-		System.out.println(dao.userRead("CDH"));
-	}
->>>>>>> master
 }
