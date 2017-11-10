6e6ff5075264907d1d4c7cec0bf9e634a568e2f6
August <dazedbj@gmail.com>
2017-11-10 13:40:32 +0900

    ver 0.6
    controllers update

--- src/main/java/com/fyni/front/UserController.java
+++ src/main/java/com/fyni/front/UserController.java
@@ -1,14 +1,15 @@
 package com.fyni.front;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.fyni.user.UserDTO;
 import com.fyni.user.UserService;
@@ -22,12 +23,20 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public ResponseEntity<String> resister(@RequestBody UserDTO dto) {
-		ResponseEntity<String> entity = null;
-		service.userCreate(dto);
-		entity = new ResponseEntity<String>("Success", HttpStatus.OK);
-		return entity;
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signUp(UserDTO dto, Model model) {
+		model.addAttribute("result", service.userCreate(dto));
+		return "signup";
+	}
+
+	@RequestMapping(value = "/listuser")
+	public ModelAndView listUser() {
+		List<UserDTO> list = service.userReadAll();
+		System.out.println(list);
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("userList", list);
+		mav.setViewName("listuser");
+		return mav;
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/views/listuser.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
+
+
+</script>
+</head>
+<body>
+	<h1>test page</h1>
+	
+	<div>
+		<div>
+			유저아이디:<input type='text' name='user_ID' id='user_ID' value = ${user_ID }>
+		</div>
+		<div>
+			유저비번:<input type='text' name='user_PWD' id='user_PWD'>
+		</div>
+		<div>
+			유저닉네임:<input type='text' name='user_Nickname' id='user_Nickname'>
+		</div>
+		<div>
+			유저공개여부:<input type='number' name='user_Profile' id='user_Profile'>
+		</div>
+		<div>
+			유저위치x:<input type='text' name='location_ID' id='location_ID'><br>
+			유저위치y:<input type='text' name='location_ID' id='location_ID'>
+		</div>
+	</div>
+
+</body>
+</html>
